51eb393c05b5df48177bcad2649275b30d86d9d7
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-01-27 02:17:19 +0000

    コメント追加

--- test/test_module.c
+++ test/test_module.c
@@ -1,16 +1,18 @@
 #include <linux/module.h>
 
+// 初期化関数
 static int __init test_init(void)
 {
     printk("Init module!\n");
     return 0;
 }
 
+// 終了関数
 static void __exit test_exit(void)
 {
     printk("Exit module!\n");
 }
 
-module_init(test_init);
-module_exit(test_exit);
-MODULE_LICENSE("GPL");
+module_init(test_init); // 初期化関数宣言
+module_exit(test_exit); // 終了関数宣言
+MODULE_LICENSE("GPL");  // ライセンス宣言
